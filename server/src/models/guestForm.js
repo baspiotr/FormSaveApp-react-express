8c0ba611dcdae5f770c433caac3300017fe433fb
Piotr Basiński <baspiotr7@gmail.com>
2019-09-19 15:22:17 +0200

    add guestForm model with mongoose & joi

---
+++ server/src/models/guestForm.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const guestFormSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    eventDate: {
+        type: Date,
+    },
+});
+
+const validateGuestForm = function(body) {
+    const schema = {
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        email: Joi.email().required(),
+        eventDate: Joi.date().required(),
+    };
+
+    return Joi.validate(body, schema);
+};
+
+const GuestForm = mongoose.model('guest_form', guestFormSchema);
+
+export { GuestForm, validateGuestForm };
